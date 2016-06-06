a3a9554f9fd2c5753bab847761f1302fb8202b72
GT3CH1 <gavinpease@gmail.com>
2016-06-06 16:39:29 -0600

    Finished seperate zone time control

--- lib/sys.py
+++ lib/sys.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python
+dir = "/var/www/html/data/sys"
 import RPi.GPIO as GPIO
 import time
+a = 0 
 GPIO.setmode(GPIO.BCM)
 pin = [13,18,23,17,27,22,10,9,11,19]
 for i in pin:
     GPIO.setup(i, GPIO.OUT)
-    print i
     GPIO.output(i, False)
-    time.sleep(300)
+    a = a + 1
+    data = open(dir +str(a)+".dat");
+    test = data.read();
+    time.sleep(float(test));
     GPIO.output(i, True)
 quit()
