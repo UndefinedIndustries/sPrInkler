cdfca6e1da47114c168a8b61cc2768f18ab9512c
GT3CH1 <gavin.pease@gmail.com>
2017-03-18 16:08:19 -0600

    Fixed more things

--- lib/version.php
+++ lib/version.php
@@ -1,5 +1,9 @@
 <?php 
 $version = file_get_contents('../version.txt');
 $outversion = file_get_contents('https://raw.githubusercontent.com/UndefinedIndustries/sPrInkler/master/version.txt');
-echo "Version $version";
+if ($version < $outversion){
+    echo "Update available";
+}else{
+echo "Version $version";    
+}
  ?>
