18672c40d0c47ed5e3535764e6c8fe7b29821790
GT3CH1 <gavin.pease@gmail.com>
2017-03-18 16:16:56 -0600

    Working on updater

--- lib/version.php
+++ lib/version.php
@@ -2,7 +2,7 @@
 $version = file_get_contents('../version.txt');
 $outversion = file_get_contents('https://raw.githubusercontent.com/UndefinedIndustries/sPrInkler/master/version.txt');
 if ($version < $outversion){
-    echo "Update available";
+    echo "Update available. click <a href='updater.php'>here</a> to update";
 }else{
 echo "Version $version";    
 }
