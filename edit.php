de0f3b3e213c557f7cd4aa33c5f424d924bc5555
GT3CH1 <gavin.pease@gmail.com>
2017-03-19 11:18:00 -0600

    Css is demon spawn.

--- edit.php
+++ edit.php
@@ -9,7 +9,7 @@
  <script src="https://gavinscodetest.tk/js/sprinkler.js"></script>
 <center>
   <br/>
-  System Timing
+  <span style="color: #fff; font-size: 1.5em">System Timing</span>
   <br/>
 <input type="submit" name="sub1" value="Submit" class="w3-btn w3-xlarge w3-blue w3-round-large w3-hover-blue-grey">
 <a href="./" style="text-decoration: none;color :#000;"><button type="button" class="w3-btn w3-xlarge w3-blue w3-round-large w3-hover-blue-grey">Cancel</button></a>
