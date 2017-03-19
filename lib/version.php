cb82c7a8f289b4831b88e7d992364b2817525705
GT3CH1 <gavin.pease@gmail.com>
2017-03-19 10:06:51 -0600

    Forgot a space

--- lib/version.php
+++ lib/version.php
@@ -2,7 +2,7 @@
 $version = file_get_contents('../version.txt');
 $outversion = file_get_contents('https://raw.githubusercontent.com/UndefinedIndustries/sPrInkler/master/version.txt?'.time());
 if ($version < $outversion){
-    echo "Version $version- Update available. click <a href='updater.php'>here</a> to update";
+    echo "Version $version - Update available. click <a href='updater.php'>here</a> to update";
 }else{
 echo "Version $version";    
 }
