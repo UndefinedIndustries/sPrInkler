37b0622a0bb3939fc2de6a977fd64ea6bab019d1
GT3CH1 <gavin.pease@gmail.com>
2017-03-19 10:00:51 -0600

    Made the updater get the version immediately

--- lib/version.php
+++ lib/version.php
@@ -1,6 +1,6 @@
 <?php 
 $version = file_get_contents('../version.txt');
-$outversion = file_get_contents('https://raw.githubusercontent.com/UndefinedIndustries/sPrInkler/master/version.txt');
+$outversion = file_get_contents('https://raw.githubusercontent.com/UndefinedIndustries/sPrInkler/master/version.txt?'.time());
 if ($version < $outversion){
     echo "Update available. click <a href='updater.php'>here</a> to update";
 }else{
