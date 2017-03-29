89aa04c06afb1915adf28f9369cbb5828c61a18a
GT3CH1 <gavin.pease@gmail.com>
2017-03-28 23:32:26 -0600

    bump

--- lib/systemtiming.php
+++ lib/systemtiming.php
@@ -9,19 +9,20 @@
 <a href="/edit.php" style="float: left;text-decoration: none;color: #fff;font-family: UB;font-size: 1em;">
     &nbsp Schedule
 </a>
-<input type="submit" name="sub1" value="Submit" class="w3-btn w3-xlarge w3-blue w3-round-large w3-hover-blue-grey">
-    <a href="/index.php" style="text-decoration: none;color :#000;">
-        <button type="button" class="w3-btn w3-xlarge w3-blue w3-round-large w3-hover-blue-grey">
-            Cancel
-        </button>
-    </a>
-</input>
-</center>
+
+
 <br/>
 <br/>
 <div id="texts" style="margin-left: 25%;margin-right:25%;width:50%;text-align: right;padding:5px;font-size: 2em;">
     <?php
-        include 'lib/edit-form.php';
+        include 'edit-form.php';
     ?>
 </div>
+<input type="submit" name="sub1" value="Submit" class="w3-btn w3-xlarge w3-blue w3-round-large w3-hover-blue-grey"></input>
+<a href="/index.php" style="text-decoration: none;color :#000;">
+    <button type="button" class="w3-btn w3-xlarge w3-blue w3-round-large w3-hover-blue-grey">
+        Cancel
+    </button>
+</a>
 </form>
+</center>
