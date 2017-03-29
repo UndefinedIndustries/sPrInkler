2d6c203434359fe5f221ad5c03c1458425eeb542
GT3CH1 <gavin.pease@gmail.com>
2017-03-28 23:37:25 -0600

    fixed more links

--- edit.php
+++ edit.php
@@ -7,7 +7,7 @@
  	<script src="https://gavinscodetest.tk/js/sprinkler.js"></script>
 	<center>
 		<br/>
-		    <a href="/lib/systemtiming.php" style="float: left;text-decoration: none;color: #fff;font-family: UB;font-size: 1em;">
+		    <a href="lib/systemtiming.php" style="float: left;text-decoration: none;color: #fff;font-family: UB;font-size: 1em;">
         		&nbspSystem Timing
         	</a>
  	        <br/>
