4f762c725721d39c31707d3b502fe2d89b3c71f9
GT3CH1 <gavinpease@gmail.com>
2017-03-31 19:58:43 -0600

    Made the buttons work nicely

--- index.php
+++ index.php
@@ -9,34 +9,38 @@
     	<link href="https://gavinscodetest.tk/css/style.css" rel="stylesheet" type="text/css"></link>
      	<script src="https://gavinscodetest.tk/js/1jquery.js"></script>
      	<script src="https://gavinscodetest.tk/js/sprinkler.js"></script>
+
     	<link rel="stylesheet" href="https://www.w3schools.com/w3css/3/w3.css">
      	<title></title>
    </head>
    <body>
-     <a href="edit.php" style="float: left;text-decoration: none;color: #fff;font-family: UB;font-size: 1em;">
+     <div style="position:fixed;top:0;left:0;">
+	<a href="edit.php" style="float: left;text-decoration: none;color: #fff;font-family: UB;font-size: 1em;">
 		 	&nbspConfig
 	</a>
 	<br/>
     <a href="../../" style="float: left;text-decoration: none;color: #fff;font-family: UB;font-size: 1em;">
 		&nbspHome
 	</a>
+	</div>
     <center>
-    	<div style="max-width: 400px;margin: 0 auto !important;float: none !important;text-align:center;">
-     		<form id ="test" action="lib/submit.php" method="post">
+    	<div style="max-width: 400px;margin-top: 50px; !important;float: none !important;text-align:center;">
+     		<form id ="test" action="lib/submit.php" method="get" onsubmit="return false;">
+        
 				<div style="max-width:600px;">
        			<?php
        				$test1=file_get_contents('lib/sys.dat'); //get the contents of the file lib/sys.dat
        				if($test1 == 1){  // if the variable test1 is equal to one
        					echo '<p style="float:left" >System Schedule <br> Status: On</p>'; //echo that the system is on
-       					echo '<button name="sysoff"style="float:right" class="w3-btn w3-teal w3-xlarge w3-hover-indigo w3-round-large"> Turn Off </button><br/><br>'; //make a buton that says turn off
+       					echo '<button name="sysoff" id="sysoff" style="float:right" class="w3-btn w3-teal w3-xlarge w3-hover-indigo w3-round-large"> Turn Off </button><br/><br>'; //make a buton that says turn off
    					}else{ //else
 				       echo '<p style="float:left">System Schedule <br> Status: Off</p>'; //echo that the system is off
-				       echo '</br><button name="syson" style="float:right" class="w3-btn w3-blue w3-xlarge w3-round-large w3-hover-indigo"> Turn On </button><br/><br>'; //make a button that says turn on
+				       echo '<button name="syson" id="syson" style="float:right" class="w3-btn w3-blue w3-xlarge w3-round-large w3-hover-indigo"> Turn On </button><br/><br>'; //make a button that says turn on
        				}
         		?>
 			</div>
      		</form>
-     		<form  action="lib/submit.php" method="post">
+     		<form  id="foo" action="lib/submit.php" method="get" onsubmit="return false;">
        			<div id="data"/>
             		<?php
 						include 'lib/check.php';
--- lib/check.php
+++ lib/check.php
@@ -1,4 +1,5 @@
-<?php
+<br>
+<br><?php
 	$a = '';
 #System 1
 	exec ( "gpio read 23", $status );
@@ -26,13 +27,28 @@
 			<div style="max-width: 400px;">
 		<?php
   		if ($value == 1){ $data = "Off"; }else{ $data = "On"; }
-   			echo '<br/><p style="float:left;" class="label">Station: '.++$a.' &nbspStatus: '.$data.'</p>';
+   			echo '<br/><p style="float:left;" class="label">Station: '.++$a.' <br>&nbspStatus: '.$data.'</p>';
    		if ($value == 0){
-     		echo '<button style="float:right;" name="off" value='.$a.' class="w3-btn w3-xlarge w3-black w3-round-large w3-hover-blue-grey" id="f-btn'.$a.'"> Turn Off </button>';
+     		echo '<button style="float:right;" name="off" value='.$a.' class="w3-btn w3-xlarge w3-black w3-round-large w3-hover-blue-grey" id="'.$a.'" onclick="getData('.$a.')">Turn Off </button><br><br>';
    		}else{
-     		echo '<button name="on" style="float:right;" value='.$a.' class="w3-btn w3-xlarge w3-black w3-round-large w3-hover-blue-grey" id="f-btn'.$a.'">Turn On </button><br><br>';
+     		echo '<button name="on" style="float:right;" value='.$a.' class="w3-btn w3-xlarge w3-black w3-round-large w3-hover-blue-grey" id="'.$a.'" onclick="getData('.$a.')">Turn On </button><br><br>';
    		}?>
-	</div>
+<br>	</div>
 		<?php
 	}
  ?>
+
+<script>
+function getData(sys){
+    var xhttp = new XMLHttpRequest();
+    var test = document.getElementById(sys).name;
+    
+    var info=test+"="+sys;
+    
+    xhttp.open("GET", "/modules/sPrInkler/lib/submit.php?"+info, true);
+    console.log("sending");
+    console.log(info);
+    xhttp.send();
+
+}
+</script>
